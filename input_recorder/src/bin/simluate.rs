10d1cdd3b119e4bc7b7654f1d181fd86e0c2983a
Thomas K Cameron <thomas.k.cameron.1218@gmail.com>
2024-05-24 03:56:24 +0900

    asdf

--- input_recorder/src/bin/simluate.rs
+++
@@ -1,7 +0,0 @@
-use input_recorder::replay;
-
-fn main() {
-    let s = include_str!("../../helloworld.json");
-    let slice = serde_json::from_str(s).unwrap();
-    replay(slice);
-}
--- input_recorder/src/lib.rs
+++ input_recorder/src/lib.rs
@@ -1,59 +1,82 @@
 use std::{
-    fs,
-    sync::{
+    borrow::Cow, fs::{self, File}, sync::{
+        atomic::AtomicBool,
         mpsc::{self, Receiver},
-    },
-    time::SystemTime,
+    }, time::{SystemTime, UNIX_EPOCH}
 };
 
 use rdev::{Event, EventType, Key};
 
 pub fn main() {
-    let rx = listen2input();
+    let mut check = AtomicBool::new(false);
+    let (tx, rx) = mpsc::channel();
+    std::thread::spawn(move || {
+        let mut func = || {
+            let tx = tx.clone();
+            let mut check = false;
+            rdev::listen(move |event| {
+                let _ = tx.send((event.time, event));
+            })
+            .unwrap();
+        };
+        
+        if cfg!(target_os = "macos") {
+            loop {
+                func();
+            }
+        } else {
+            func();
+        }
+    });
 
     let th = std::thread::spawn(move || {
         let mut stack = Vec::with_capacity(1024 * 10);
         let mut stack2 = vec![];
-        while let Ok((_time, e)) = rx.recv() {
+        while let Ok((time, e)) = rx.recv() {
             if e.event_type == EventType::KeyPress(Key::F2) {
                 break;
             }
-            stack.push(e.event_type);
+            stack.push((time, e.event_type));
             if stack.len() == stack.capacity() {
-                stack.dedup();
+                stack.dedup_by_key(|a| a.1);
                 stack2.extend_from_slice(&stack);
                 stack.clear();
             }
         }
-        stack.dedup();
+
         stack2.extend_from_slice(&stack);
+        stack2.dedup();
         stack2
     });
 
     let mut s = th.join().unwrap();
     s.dedup();
     let slice = s.into_boxed_slice();
-    let s = serde_json::to_string(&slice).unwrap();
-    fs::write("./asdf.json", &s).unwrap();
+
+    let mut file_path = uuid::Uuid::new_v4().to_string();
+    let mut fp = File::create(file_path).unwrap();
+
+
+    let mut s = "".to_string();
+    for i in slice {
+        let mut jsonstr = serde_json::to_string(&i).unwrap();
+        s.push_str(&jsonstr);
+        s.push('\n');
+        s.shrink_to_fit();
+    }
+    
+    zstd::stream::copy_encode(std::io::Cursor::new(jsonstr), fp, 5);
+    
 }
 
-fn listen2input() -> Receiver<(SystemTime, Event)> {
-    let (tx, rx) = mpsc::channel();
-    std::thread::spawn(move || loop {
-        let tx = tx.clone();
-        rdev::listen(move |event| {
-            let _ = tx.send((event.time, event));
-        })
-        .unwrap();
-    });
-
-    rx
-}
-
-pub fn replay(slice: Box<[EventType]>) {
-    for i in slice.iter() {
-        rdev::simulate(i).unwrap();
+pub fn replay(slice: Box<[(SystemTime, EventType)]>) {
+    let mut prev = slice.iter().next().map(|i| i.0).unwrap();
+    for (time, e_ty) in slice.iter() {
+        std::thread::sleep(time.duration_since(prev).unwrap());
+        rdev::simulate(e_ty).unwrap();
     }
 }
 
-struct RDev {}
+pub fn stream_file() {
+    
+}
