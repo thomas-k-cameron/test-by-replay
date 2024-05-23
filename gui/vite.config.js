27aaa2e55c7b4e33822931268205cf71d4e49715
Thomas K Cameron <thomas.k.cameron.1218@gmail.com>
2024-05-24 04:02:28 +0900

    gui init

--- gui/vite.config.js
+++ gui/vite.config.js
@@ -13,8 +13,9 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
   },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
 }));
